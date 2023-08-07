8efce0b9c2d2483ab331f796049c1663da3ac5be
TanishqSingla <tanishqsingla00@gmail.com>
2023-08-08 00:49:07 +0530

    chore: Nodejs initial setup
    Install express
    Setup env variables
    Setup express
    Add env example file

---
+++ node-express/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+if(process.env.NODE_ENV) {
+	dotenv.config({path: `.env.${process.env.NODE_ENV}`});
+} else {
+	dotenv.config();
+}
+
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+	if(process.env.NODE_ENV === 'development') {
+		console.log(`Server is running on http://localhost:${process.env.PORT}`);  
+		return;
+	}
+  
+	console.log(`Server is running on https://${process.env.BASE_URL}:${process.env.PORT}/`);
+});
