f346413cb6c17e6160525fb94258bdf4e06eedff
TanishqSingla <tanishqsingla00@gmail.com>
2023-08-10 10:31:30 +0530

    feat: Add prettier setup

--- node-express/index.js
+++ node-express/index.js
@@ -11,8 +11,11 @@ const app = express();
 app.use(express.json());
 
 app.listen(process.env.PORT, () => {
-	if (!process.env.NODE_ENV) { //  Catching for falsy emtpy string ''
-		console.log(`Server is running on https://${process.env.BASE_URL}:${process.env.PORT}/`);
+	if (!process.env.NODE_ENV) {
+		//  Catching for falsy emtpy string ''
+		console.log(
+			`Server is running on https://${process.env.BASE_URL}:${process.env.PORT}/`,
+		);
 		return;
 	}
 
