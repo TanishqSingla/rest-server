68eb417e7d5cd30c88441573453834cec36d1353
TanishqSingla <tanishqsingla00@gmail.com>
2023-08-11 14:45:38 +0530

    fix: env file condition

--- node-express/index.js
+++ node-express/index.js
@@ -11,10 +11,10 @@ const app = express();
 app.use(express.json());
 
 app.listen(process.env.PORT, () => {
-	if (process.env.NODE_ENV) { //  Catching for falsy emtpy string ''
+	if (!process.env.NODE_ENV) { //  Catching for falsy emtpy string ''
 		console.log(`Server is running on https://${process.env.BASE_URL}:${process.env.PORT}/`);
 		return;
 	}
 
-	console.log(`Server is running on http://localhost:${process.env.PORT}`);
+	console.log(`Server is running on http://${process.env.BASE_URL}:${process.env.PORT}`);
 });
